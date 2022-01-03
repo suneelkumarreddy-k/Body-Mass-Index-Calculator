ff9379033efabe02a12af16c5c40307e7d1d81d3
suneelkumarreddy-k <67097067+suneelkumarreddy-k@users.noreply.github.com>
2022-01-03 12:13:19 +0530

    Update app.js
    Code changed

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ var start = performance.now();
 let score = document.getElementById("weight").value / (document.getElementById("height").value * 2);
 document.getElementById("score").innerHTML = score;
 switch (score) {
-  case score < 18.5:
-    document.getElementById("result").innerHTML = "Underweight";
-    break;
-  case score >= 18.5 && score <= 25: 
-    document.getElementById("result").innerHTML = "Normal (healthy weight)";
-    break;
-  case score >= 25 && score <= 30:
+case score >30:
+    document.getElementById("result").innerHTML = "Obese";
+break; 
+case score >= 25 && score <= 30:
      document.getElementById("result").innerHTML = "Overweight";
     break;
-  case score >30:
-    document.getElementById("result").innerHTML = "Obese";
+case score >= 18.5 && score <= 25: 
+    document.getElementById("result").innerHTML = "Normal (healthy weight)";
+    break;
+default :
+    document.getElementById("result").innerHTML = "Underweight";
 }
 var end = performance.now();
 var wpn = end-start;
