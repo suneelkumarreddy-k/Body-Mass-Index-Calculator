642bd14bcd3fba86f1ecbc5cb9a581e0cb7eda1a
suneelkumarreddy-k <67097067+suneelkumarreddy-k@users.noreply.github.com>
2022-01-03 11:28:04 +0530

    Update app.js
    Code changed

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 function bmi() {
 var start = performance.now();
-document.getElementById("score").innerHTML = document.getElementById("weight").value / (document.getElementById("height").value * 2);
+let score = document.getElementById("weight").value / (document.getElementById("height").value * 2);
+document.getElementById("score").innerHTML = score;
+switch (score) {
+  case score < 18.5:
+    document.getElementById("result").innerHTML = "Underweight";
+    break;
+  case score >= 18.5 && score <= 25: 
+    document.getElementById("result").innerHTML = "Normal (healthy weight)";
+    break;
+  case score >= 25 && score <= 30:
+     document.getElementById("result").innerHTML = "Overweight";
+    break;
+  case score >30:
+    document.getElementById("result").innerHTML = "Obese";
+}
 var end = performance.now();
 var wpn = end-start;
 alert("Calculated in "+wpn+" ms 😎");
