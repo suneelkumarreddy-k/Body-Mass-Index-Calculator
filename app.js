439acb4dd0237c9f01c7dd7574e4a84c150fd9f8
suneelkumarreddy-k <67097067+suneelkumarreddy-k@users.noreply.github.com>
2022-01-03 11:08:55 +0530

    Update app.js
    Code changed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function bmi() {
 var start = performance.now();
-document.getElementById("score").innerHTML = Math.ceil(document.getElementById("weight").value / (document.getElementById("height").value * 2));
+document.getElementById("score").innerHTML = document.getElementById("weight").value / (document.getElementById("height").value * 2);
 var end = performance.now();
 var wpn = end-start;
 alert("Calculated in "+wpn+" ms 😎");
