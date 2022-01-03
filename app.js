b11dd1e25848d7d04cdd5379a7676294c1005388
suneelkumarreddy-k <67097067+suneelkumarreddy-k@users.noreply.github.com>
2022-01-03 13:14:29 +0530

    Update app.js
    Code changed

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ function bmi() {
 var start = performance.now();
 let score = document.getElementById("weight").value / (document.getElementById("height").value * 2);
 document.getElementById("score").innerHTML = score.toFixed(2);
-
+document.getElementById("result").innerHTML = score >30 ?  "Obese" : score > 25 && score <= 30 ? "Overweight" : score >= 18.5 && score <= 25 ? "Normal (healthy weight)" : "Underweight";  
+/*
 if(score >30) {
     document.getElementById("result").innerHTML = "Obese";
 }
@@ -17,6 +18,7 @@ else if(score >= 18.5 && score <= 25) {
 else {
     document.getElementById("result").innerHTML = "Underweight";
 }
+*/
 var end = performance.now();
 var wpn = end-start;
 alert("Calculated in "+wpn+" ms 😎");
