7f63d37dec13e9db43c13186e99ca252ecaef353
suneelkumarreddy-k <67097067+suneelkumarreddy-k@users.noreply.github.com>
2022-01-05 10:49:02 +0530

    Update app.js
    Code changed

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // Author: Shri Suneel Kumar Reddy 
 function te(ob) {
 if(ob.id == "sre") {
-  const cb = navigator.clipboard;
-  const p = document.getElementById("score").innerHTML;
-  cb.writeText(p).then(() => alert('Text copied'));
+    var p = document.getElementById("score").innerHTML;  
+navigator.clipboard.writeText(p);
+alert('Text copied');
 } else {
-const cb = navigator.clipboard;
-  const p = document.getElementById("result").innerHTML;
-  cb.writeText(p).then(() => alert('Text copied'));
+  var p = document.getElementById("result").innerHTML;
+  navigator.clipboard.writeText(p);
+alert('Text copied');
 }
 }
 
